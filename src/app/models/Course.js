636a6d22b21e3ffbbf153ad670b50bcdd444c8cf
truong thinh <thinh2k1310@gmail.com>
2022-03-06 22:44:40 +0700

    Create Course

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const slug = require('mongoose-slug-generator')
+mongoose.plugin(slug)
 // const ObjectId = Schema.ObjectId;
 
 const Course = new Schema({
@@ -7,7 +9,10 @@ const Course = new Schema({
   name: String,
   description: String,
   image: String,
-  createdAt: {type: Date, default: Date.now},
-  updatedAt: {type: Date, default: Date.now}
+  videoId: String,
+  level: String,
+  slug : {type: String, slug: 'name' , unique: true}
+},{
+  timestamps: true,
 });
 module.exports =  mongoose.model('Course', Course);
--- src/routes/courses.js
+++ src/routes/courses.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const courseController = require('../app/controllers/CourseController');
+const { route } = require('./news');
 
-router.get('/:slug', courseController.show)
+router.get('/create', courseController.create);
+router.post('/store', courseController.store);
+
+router.get('/:slug', courseController.show);
 
 
 
